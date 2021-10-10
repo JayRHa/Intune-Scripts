28346589ee68e35220e87af97ecd04da352df136
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2021-10-10 17:28:22 +0200

    Rename Script

--- Delete-MsTeamsClient.ps1
+++ Delete-MsTeamsClient.ps1
@@ -6,7 +6,7 @@ Description:
 Delete the build-in Windows 11 teams client with help of intune
 Release notes:
 Version 1.0: Init
-#> 
+#>  
 
 
 Get-AppxPackage | Where-Object Name -like "*MicrosoftTeams*" | Remove-AppxPackage
