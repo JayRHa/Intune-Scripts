270a29ef5f5b7b2be7232384832985556b0942f9
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2021-10-10 17:24:51 +0200

    add Delete-TeamsClient

---
+++ delete-TeamsClient.ps1
@@ -0,0 +1,12 @@
+<#
+Version: 1.0
+Author: Jannik Reinhard (jannikreinhard.com)
+Script: Delete-TeamsClient
+Description:
+Delete the build-in Windows 11 teams client with help of intune
+Release notes:
+Version 1.0: Init
+#> 
+
+
+Get-AppxPackage | Where-Object Name -like "*MicrosoftTeams*" | Remove-AppxPackage
