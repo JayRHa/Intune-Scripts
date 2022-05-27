de06de27a0c18942bf3404707d906ff9509f0ee6
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2022-05-27 18:47:18 +0200

    Init Change-ImeLogLevel

---
+++ Change-ImeLogLevel/Change-ImeLogLevel.ps1
@@ -0,0 +1,28 @@
+<#
+Version: 1.0
+Author: Jannik Reinhard (jannikreinhard.com)
+Script: Change-ImeLogLevel
+Description:
+Change the loglevel from the Intune management extension
+Release notes:
+Version 1.0: Init
+#> 
+
+$logLevelSelection = Read-Host "Enter the log level [Critical, Error, Warning, Information, Verbose]"
+while("Critical", "Error", "Warning", "Information", "Verbose" -notcontains $logLevelSelection )
+{
+    $logLevelSelection = Read-Host "Enter the log level [Critical, Error, Warning, Information, Verbose]"
+}
+
+$imeConfFile = "C:\Program Files (x86)\Microsoft Intune Management Extension\Microsoft.Management.Services.IntuneWindowsAgent.exe.config"
+#$configFile = Get-Content -Path $imeConfFile
+
+
+$configFile = New-Object System.XML.XMLDocument
+$configFile.Load($imeConfFile)
+
+$logLevel = $configFile.configuration.'system.diagnostics'.sources.source
+$logLevel.switchValue = "$logLevelSelection"
+$configFile.Save($imeConfFile)
+
+Restart-Service -DisplayName "Microsoft Intune Management Extension"
