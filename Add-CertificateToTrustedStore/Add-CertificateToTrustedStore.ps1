ef9d816fbc92d2ae8f8b33557ddb06512e9eeb21
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2022-05-29 13:03:01 +0200

    Init Add-CertificateToTrustedStore

---
+++ Add-CertificateToTrustedStore/Add-CertificateToTrustedStore.ps1
@@ -0,0 +1,112 @@
+<#
+Version: 1.0
+Author: Jannik Reinhard (jannikreinhard.com)
+Script: Add-CertificateToTrustedStore
+Description:
+Create config profile to import certificate to the truste cer store
+Release notes:
+Version 1.0: Init
+#>
+
+function Get-Certificate {
+    #Select the cer file
+    Add-Type -AssemblyName System.Windows.Forms
+    $fileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+    $fileBrowser.filter = "Certificate (*.cer)| *.cer"
+    [void]$fileBrowser.ShowDialog()
+    return $fileBrowser.FileName
+}
+
+function Get-AuthToken {
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        $User
+    )
+
+    $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+    $tenant = $userUpn.Host
+    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
+    if ($AadModule -eq $null) {
+        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
+        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+    }
+
+    $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+    $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+
+    [System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+    [System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+    $clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    $resourceAppIdURI = "https://graph.microsoft.com"
+    $authority = "https://login.microsoftonline.com/$Tenant"
+
+    $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+    $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
+    $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
+    $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
+
+      
+    $authHeader = @{
+        'Content-Type'='application/json'
+        'Authorization'="Bearer " + $authResult.AccessToken
+        'ExpiresOn'=$authResult.ExpiresOn
+        }
+
+    return $authHeader
+
+}
+
+function Import-ConfigurationProfile {
+    param(
+             [Parameter(Mandatory)]
+             $ConfigProfile
+       )
+    #$profile = $ConfigProfile | Select-Object -Property * -ExcludeProperty id,createdDateTime,lastModifiedDateTime,version,supportsScopeTags
+    #$profile = $ConfigProfile | ConvertTo-Json
+    Write-Host $ConfigProfile
+    Invoke-RestMethod -Uri https://graph.microsoft.com/v1.0/deviceManagement/deviceConfigurations -Headers $authToken -Method Post -Body $ConfigProfile -ContentType "application/json" 
+}
+
+
+# Get name of the policy
+$confProfileName = Read-Host "Enter a name for the configuration profile"
+
+# Get certificate
+$certificatePath = Get-Certificate
+
+# Get needed informations
+$certThumbprint = ([System.Security.Cryptography.X509Certificates.X509Certificate2]::new($certificatePath)).thumbprint
+$encodeCertificate = [System.Convert]::ToBase64String(([System.Security.Cryptography.X509Certificates.X509Certificate2]::new($certificatePath)).Export('Cert'), 'InsertLineBreaks')
+$fileName = Split-Path $certificatePath -leaf
+$omaUri = "./Device/Vendor/MSFT/RootCATrustedCertificates/TrustedPublisher/$certThumbprint/EncodedCertificate"
+
+$customConfigProfile = @"
+{
+    "@odata.type": "#microsoft.graph.windows10CustomConfiguration",
+    "description": "",
+    "displayName": "$confProfileName",
+    "omaSettings": [
+        {
+            "@odata.type": "#microsoft.graph.omaSettingString",
+            "displayName": "$fileName",
+            "description": "",
+            "omaUri": "$omaUri",
+            "value":  "$encodeCertificate"
+        }
+    ]
+}
+"@
+
+#Auth
+if(-not $global:authToken){
+    if($User -eq $null -or $User -eq ""){
+    $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+    Write-Host
+    }
+    $global:authToken = Get-AuthToken -User $User
+}
+
+Import-ConfigurationProfile  -ConfigProfile $customConfigProfile
