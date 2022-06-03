92b40484bc77bd56279f5b29e6395ea5f511e231
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2022-06-03 12:20:19 +0200

    Init Hide-TaskViewAndWidgetsDetection

---
+++ Hide-TaskViewAndWidgets/Hide-TaskViewAndWidgetsRemediation.ps1
@@ -0,0 +1,43 @@
+<#
+Version: 1.0
+Author: Jannik Reinhard (jannikreinhard.com)
+Script: Hide-TaskViewAndWidgetsDetection
+Description:
+Hite the Task View and Widgets icons in the task bar
+Release notes:
+Version 1.0: Init
+#> 
+
+
+function Test-RegistryValue {
+    param (
+     [parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]$Path,
+    
+    [parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]$Value
+    )
+    
+    try {
+        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+        return $true
+    }catch {
+        return $false
+    }
+}
+
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$showTaskViewButton = "ShowTaskViewButton"
+$showWidgets = "TaskbarDa"
+
+If(Test-RegistryValue -Path $regPath -Value $showTaskViewButton){
+    if((Get-ItemProperty -Path $regPath | Select-Object -ExpandProperty $showTaskViewButton) -ne 0){
+        Set-ItemProperty -path $regPath -name $showTaskViewButton -value 0
+    }
+}
+
+If(Test-RegistryValue -Path $regPath -Value $showTaskViewButton){
+    if((Get-ItemProperty -Path $regPath | Select-Object -ExpandProperty $showWidgets) -ne 0){
+        Set-ItemProperty -path $regPath -name $showWidgets -value 0
+    }
+}
