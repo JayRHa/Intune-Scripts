84f6bac50306a7dae1739cfd49b287a28bbae5d3
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2022-06-06 12:39:08 +0200

    minor updates

--- Change-ImeLogLevel/Change-ImeLogLevel.ps1
+++ Change-ImeLogLevel/Change-ImeLogLevel.ps1
@@ -8,32 +8,45 @@ Release notes:
 Version 1.0: Init
 #> 
 
+# Check if Admin
 If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
 	Write-Warning "Please execute the script with admin rights"
     exit
 }
 
+Function Change-ImeLoglevel($logLevelValue){
+    $imeConfFile = "C:\Program Files (x86)\Microsoft Intune Management Extension\Microsoft.Management.Services.IntuneWindowsAgent.exe.config"
+
+    $configFile = New-Object System.XML.XMLDocument
+    $configFile.Load($imeConfFile)
+
+    $logLevel = $configFile.configuration.'system.diagnostics'.sources.source
+    $logLevel.switchValue = "$logLevelValue"
+    $configFile.Save($imeConfFile)
+    Write-Warning "IME Log level changed to $logLevelValue"
+}
+
+Function Restart-Ime{
+    Restart-Service -DisplayName "Microsoft Intune Management Extension"
+    Write-Warning "IME Service was restarted"    
+}
+
+# Select Logging level
 $logLevelSelection = Read-Host "Enter the log level [Critical, Error, Warning, Information, Verbose]"
 while("Critical", "Error", "Warning", "Information", "Verbose" -notcontains $logLevelSelection )
 {
     $logLevelSelection = Read-Host "Enter the log level [Critical, Error, Warning, Information, Verbose]"
 }
 
-$imeConfFile = "C:\Program Files (x86)\Microsoft Intune Management Extension\Microsoft.Management.Services.IntuneWindowsAgent.exe.config"
+# Change logging level
+Change-ImeLoglevel($logLevelSelection)
 
-$configFile = New-Object System.XML.XMLDocument
-$configFile.Load($imeConfFile)
-
-$logLevel = $configFile.configuration.'system.diagnostics'.sources.source
-$logLevel.switchValue = "$logLevelSelection"
-$configFile.Save($imeConfFile)
-Write-Warning "IME Log level changed to $logLevelSelection"
-
-Restart-Service -DisplayName "Microsoft Intune Management Extension"
-Write-Warning "IME Service was restarted"
+# Restart IME Service
+Restart-Ime
 
 
+# Open Ime log folder
 $openLogs = Read-Host "Open Log file folder [Y/N]"
 while("Y", "N", "Yes", "No", "y", "n" -notcontains $openLogs )
 {
