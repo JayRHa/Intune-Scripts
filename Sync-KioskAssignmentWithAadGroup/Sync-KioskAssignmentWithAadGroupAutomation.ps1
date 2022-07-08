6e05455622c9ecefbbb54bc1d296b6d1e531642f
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2022-07-08 20:01:58 +0200

    init

--- Sync-KioskAssignmentWithAadGroup/Sync-KioskAssignmentWithAadGroupAutomation.ps1
+++ Sync-KioskAssignmentWithAadGroup/Sync-KioskAssignmentWithAadGroupAutomation.ps1
@@ -8,47 +8,29 @@ Release notes:
 Version 1.0: Init
 #>
 
-function Get-AuthToken {
-    [cmdletbinding()]
-    param
-    (
-        [Parameter(Mandatory=$true)]
-        $User
-    )
+Function Get-AuthHeader{
+    $tenantId = 'ad7cb8fd-339e-438e-8db7-0168697e33f2'
+    $clientId = '2aa220cc-f720-4b56-abfe-fecd8031c278'
+    $clientSecret = 'fDM8Q~kgNYRGOV9xeBlDR-7U0tuIIWme5GiG6aYN'
 
-    $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
-    $tenant = $userUpn.Host
-    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
-    if ($AadModule -eq $null) {
-        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
-        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+
+    $connectionDetails = @{
+        'tenant'     	= $tenantId
+        'client_id'  	= $clientId
+        'scope'	 	 	= 'https://graph.microsoft.com/.default'
+        'client_secret' = $clientSecret
+        'grant_type'	= 'client_credentials'
     }
 
-    $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
-    $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
-
-    Add-Type -Path $adal
-    Add-Type -Path $adalforms
-    # [System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
-    # [System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
-    $clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
-    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
-    $resourceAppIdURI = "https://graph.microsoft.com"
-    $authority = "https://login.microsoftonline.com/$Tenant"
-
-    $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
-    $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
-    $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
-    $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
-
-      
+    $response = Invoke-WebRequest -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -ContentType "application/x-www-form-urlencoded" -Body $connectionDetails -Method Post
+    
     $authHeader = @{
         'Content-Type'='application/json'
-        'Authorization'="Bearer " + $authResult.AccessToken
-        'ExpiresOn'=$authResult.ExpiresOn
+        'Authorization'="Bearer " + ($response.content | ConvertFrom-Json).access_token
         }
 
     return $authHeader
+
 }
 
 function Get-GraphCall {
@@ -68,14 +50,7 @@ function Get-GraphCall {
 $profileId = ''
 $groupId = ''
 
-#Auth
-if(-not $global:authToken){
-    if($User -eq $null -or $User -eq ""){
-    $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
-    Write-Host
-    }
-    $global:authToken = Get-AuthToken -User $User
-}
+
 
 $kioskProfile  = Get-GraphCall -apiUri "deviceManagement/deviceConfigurations/$profileId" -method GET
 $request = @'
