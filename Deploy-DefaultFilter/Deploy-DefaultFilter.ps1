4a31084acd705cc6dc735ab17f4bab456a08a9ee
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2022-07-15 14:48:59 +0200

    add description to the filter

--- Deploy-DefaultFilter/Deploy-DefaultFilter.ps1
+++ Deploy-DefaultFilter/Deploy-DefaultFilter.ps1
@@ -29,6 +29,7 @@ function Add-IntuneFilter{
     param (
         [parameter(Mandatory=$true)]$Name,
         [parameter(Mandatory=$true)]$Platform,
+        [parameter(Mandatory=$true)]$Description,
         [parameter(Mandatory=$true)]$Rule
     )
 
@@ -37,7 +38,7 @@ function Add-IntuneFilter{
     }
     $params = @{
         DisplayName = $filterPreFix + $Name
-        Description = ""
+        Description = $Description
         Platform = $Platform
         Rule = $Rule
         RoleScopeTags = @()
@@ -55,30 +56,30 @@ Get-GraphAuthentication
 
 ###### Windows 10 ######
 # Ownership
-Add-IntuneFilter -Name "AllPersonalDevices" -Platform "Windows10AndLater" -Rule '(device.deviceOwnership -eq "Personal")'
-Add-IntuneFilter -Name "AllCorporateDevices" -Platform "Windows10AndLater" -Rule '(device.deviceOwnership -eq "Corporate")'
+Add-IntuneFilter -Name "AllPersonalDevices" -Platform "Windows10AndLater" -Description "All personal W10 and later devices" -Rule '(device.deviceOwnership -eq "Personal")'
+Add-IntuneFilter -Name "AllCorporateDevices" -Platform "Windows10AndLater" -Description "All corporate W10 and later devices" -Rule '(device.deviceOwnership -eq "Corporate")'
 
 # Enrollment Profile
 Get-MgDeviceManagementWindowAutopilotDeploymentProfile | ForEach-Object {
-    Add-IntuneFilter -Name ("Enrollment"+($($_.DisplayName).Trim())) -Platform "Windows10AndLater" -Rule ('(device.enrollmentProfileName -eq "'+$($_.DisplayName)+'")' )
+    Add-IntuneFilter -Name ("Enrollment"+($($_.DisplayName).Trim())) -Platform "Windows10AndLater" -Description ("All devcies with enrollment profile"+($($_.DisplayName).Trim())) -Rule ('(device.enrollmentProfileName -eq "'+$($_.DisplayName)+'")' )
 }
 
 # Operating System SKU
 $sku = @("Education", "Enterprise", "IoTEnterprise", "Professional", "Holographic")  
 $sku | ForEach-Object {
-    Add-IntuneFilter -Name "AllSku$_" -Platform "Windows10AndLater" -Rule ('(device.operatingSystemSKU  -eq "'+$_+'")')
+    Add-IntuneFilter -Name "AllSku$_" -Platform "Windows10AndLater" -Description "All devices with SKU $_" -Rule ('(device.operatingSystemSKU  -eq "'+$_+'")')
 }
 
 # Operating System Version
-Add-IntuneFilter -Name "AllWindows11" -Platform "Windows10AndLater" -Rule '(device.osVersion -startsWith "10.0.22")'
-Add-IntuneFilter -Name "AllWindows10" -Platform "Windows10AndLater" -Rule '(device.osVersion -startsWith "10.0.1")'
-Add-IntuneFilter -Name "AllWindows8.1" -Platform "Windows10AndLater" -Rule '(device.osVersion -startsWith "6.3")'
+Add-IntuneFilter -Name "AllWindows11" -Platform "Windows10AndLater" -Description "All Windows 11 devices" -Rule '(device.osVersion -startsWith "10.0.22")'
+Add-IntuneFilter -Name "AllWindows10" -Platform "Windows10AndLater" -Description "All Windows 10 devices" -Rule '(device.osVersion -startsWith "10.0.1")'
+Add-IntuneFilter -Name "AllWindows8.1" -Platform "Windows10AndLater" -Description "All Windows 8.1 devices" -Rule '(device.osVersion -startsWith "6.3")'
 
 # Device Category
 Get-MgDeviceManagementDeviceCategory | ForEach-Object {
-    Add-IntuneFilter -Name ("Category"+($($_.DisplayName).Trim())) -Platform "Windows10AndLater" -Rule ('(device.deviceCategory  -eq "'+$($_.DisplayName)+'")' )
+    Add-IntuneFilter -Name ("Category"+($($_.DisplayName).Trim())) -Description ("All device with category "+($($_.DisplayName).Trim())) -Platform "Windows10AndLater" -Rule ('(device.deviceCategory  -eq "'+$($_.DisplayName)+'")' )
 }
 
 # Model
-Add-IntuneFilter -Name "AllCloudPCs" -Platform "Windows10AndLater" -Rule '(device.model -contains "CloudPC") or (device.model -contains "Cloud PC")'
+Add-IntuneFilter -Name "AllCloudPCs" -Platform "Windows10AndLater" -Description "All Microsoft365 devices" -Rule '(device.model -contains "CloudPC") or (device.model -contains "Cloud PC")'
 
