ff676cac304594bbf5b6bd2702a5873ebb3b9a4f
Jannik Reinhard <Jannik.Reinhard@basf.com>
2022-09-01 19:51:22 +0200

    script optimization

--- Remove-PrimaryUserFromIntuneDevices.ps1
+++ Remove-PrimaryUserFromIntuneDevices.ps1
@@ -1,139 +1,53 @@
-function Get-AuthToken {
-
 <#
-.SYNOPSIS
-This function is used to authenticate with the Graph API REST interface
-.DESCRIPTION
-The function authenticate with the Graph API Interface with the tenant name
-.EXAMPLE
-Get-AuthToken
-Authenticates you with the Graph API interface
-.NOTES
-NAME: Get-AuthToken
-#>
+Version: 1.0
+Author: Jannik Reinhard (jannikreinhard.com)
+Script: Remove-PrimaryUserFromIntuneDevices
+Description:
+Remove the rimary user fro all devices
+Release notes:
+Version 1.0: Init
+#> 
+function Get-AuthToken {
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        $User
+    )
 
-[cmdletbinding()]
-
-param
-(
-    [Parameter(Mandatory=$true)]
-    $User
-)
-
-$userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
-
-$tenant = $userUpn.Host
-
-Write-Host "Checking for AzureAD module..."
-
+    $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+    $tenant = $userUpn.Host
     $AadModule = Get-Module -Name "AzureAD" -ListAvailable
-
     if ($AadModule -eq $null) {
-
         Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
         $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
-
     }
 
-    if ($AadModule -eq $null) {
-        write-host
-        write-host "AzureAD Powershell module not installed..." -f Red
-        write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
-        write-host "Script can't continue..." -f Red
-        write-host
-        exit
-    }
+    $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+    $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
 
-# Getting path to ActiveDirectory Assemblies
-# If the module count is greater than 1 find the latest version
-
-    if($AadModule.count -gt 1){
-
-        $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
-
-        $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
-
-            # Checking if there are multiple versions of the same module found
-
-            if($AadModule.count -gt 1){
-
-            $aadModule = $AadModule | select -Unique
-
-            }
-
-        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
-        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
-
-    }
-
-    else {
-
-        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
-        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
-
-    }
-
-[System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
-
-[System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
-
-$clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
-
-$redirectUri = "urn:ietf:wg:oauth:2.0:oob"
-
-$resourceAppIdURI = "https://graph.microsoft.com"
-
-$authority = "https://login.microsoftonline.com/$Tenant"
-
-    try {
+    Add-Type -Path $adal
+    Add-Type -Path $adalforms
+    # [System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+    # [System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+    $clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    $resourceAppIdURI = "https://graph.microsoft.com"
+    $authority = "https://login.microsoftonline.com/$Tenant"
 
     $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
-
-    # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
-    # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
-
     $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
-
     $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
-
     $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
 
-        # If the accesstoken is valid then create the authentication header
-
-        if($authResult.AccessToken){
-
-        # Creating header for Authorization token
-
-        $authHeader = @{
-            'Content-Type'='application/json'
-            'Authorization'="Bearer " + $authResult.AccessToken
-            'ExpiresOn'=$authResult.ExpiresOn
-            }
-
-        return $authHeader
-
+      
+    $authHeader = @{
+        'Content-Type'='application/json'
+        'Authorization'="Bearer " + $authResult.AccessToken
+        'ExpiresOn'=$authResult.ExpiresOn
         }
 
-        else {
-
-        Write-Host
-        Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
-        Write-Host
-        break
-
-        }
-
-    }
-
-    catch {
-
-    write-host $_.Exception.Message -f Red
-    write-host $_.Exception.ItemName -f Red
-    write-host
-    break
-
-    }
-
+    return $authHeader
 }
 
 ####################################################
@@ -159,41 +73,22 @@ param
 [ValidateNotNullOrEmpty()]
 [string]$deviceName
 )
-    
-    $graphApiVersion = "beta"
 
-    try {
-
-        if($deviceName){
-
-            $Resource = "deviceManagement/managedDevices?`$filter=deviceName eq '$deviceName'"
-	        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)" 
-
-            (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).value
-
+    $devices = @()
+    $uri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
+    if($deviceName) {
+        $uri = "$uri?" + '$filter' + "=deviceName eq '$deviceName'"
+        $response = Invoke-RestMethod -Uri $uri -Headers $authToken -Method "GET"
+        $devices += $response.value
+    }else{
+        while($uri)
+        {
+            $response = Invoke-RestMethod -Uri $uri -Headers $authToken -Method "GET"
+            $devices += $response.value
+            $uri = $response.'@odata.nextLink'
         }
-
-        else {
-
-            $Resource = "deviceManagement/managedDevices?`$filter=(((deviceType%20eq%20%27desktop%27)%20or%20(deviceType%20eq%20%27windowsRT%27)%20or%20(deviceType%20eq%20%27winEmbedded%27)%20or%20(deviceType%20eq%20%27surfaceHub%27)))"
-	        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
-        
-            (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).value
-
-        }
-
-	} catch {
-		$ex = $_.Exception
-		$errorResponse = $ex.Response.GetResponseStream()
-		$reader = New-Object System.IO.StreamReader($errorResponse)
-		$reader.BaseStream.Position = 0
-		$reader.DiscardBufferedData()
-		$responseBody = $reader.ReadToEnd();
-		Write-Host "Response content:`n$responseBody" -f Red
-		Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
-		throw "Get-IntuneManagedDevices error"
-	}
-
+    }
+    return $devices
 }
 
 ####################################################
@@ -292,62 +187,26 @@ $IntuneDeviceId
 
 }
 
-####################################################
-
-#region Authentication
-
-write-host
-
-# Checking if authToken exists before running authentication
-if($global:authToken){
-
-    # Setting DateTime to Universal time to work in all timezones
-    $DateTime = (Get-Date).ToUniversalTime()
-
-    # If the authToken exists checking when it expires
-    $TokenExpires = ($authToken.ExpiresOn.datetime - $DateTime).Minutes
-
-    if($TokenExpires -le 0){
-
-        write-host "Authentication Token expired" $TokenExpires "minutes ago" -ForegroundColor Yellow
-        write-host
-
-        # Defining User Principal Name if not present
-
-        if($User -eq $null -or $User -eq ""){
-            $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
-            Write-Host
-        }
-
-        $global:authToken = Get-AuthToken -User $User
+#Auth
+if(-not $global:authToken){
+    if($User -eq $null -or $User -eq ""){
+    $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+    Write-Host
     }
-}
-
-# Authentication doesn't exist, calling Get-AuthToken function
-
-else {
-
-    if($User -eq $null -or $User -eq "") {
-        $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
-        Write-Host
-    }
-
-    # Getting the authorization token
     $global:authToken = Get-AuthToken -User $User
 }
-
-#endregion
-
-####################################################
 
 
 $allDevices = Get-Win10IntuneManagedDevices
 $filter = "*" # If nothing specified then all devices. Use wildcard e.g. *
 
-$sharedDevices = $allDevices | Where-Object {$_.deviceName -like $filter}
+if($filter -eq '*'){
+    $allDevices = $allDevices | Where-Object {$_.deviceName -like $filter}
+}
 
-Foreach ($deviceEntry in $sharedDevices){
-    Write-Host "Change $($deviceEntry.devicename) to a shared device"
-    Delete-IntuneDevicePrimaryUser -IntuneDeviceId $deviceEntry.id -ErrorAction Continue
+
+Foreach ($allDevice in $allDevices){
+    Write-Host "Change $($allDevice.devicename) to a shared device"
+    Delete-IntuneDevicePrimaryUser -IntuneDeviceId $allDevice.id -ErrorAction Continue
 }
 
