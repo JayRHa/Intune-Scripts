e9c93016a084aa06f69b23ccd5553498ab5873aa
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2022-11-28 19:56:34 +0100

    init 1.7

--- Check-AutopilotPrerequisites/Check-AutopilotPrerequisites.ps1
+++ Check-AutopilotPrerequisites/Check-AutopilotPrerequisites.ps1
@@ -33,7 +33,8 @@
   Version 1.4: Minor fixes
   Version 1.5: Add Autopilot profile info and dhcp bug fix
   Version 1.6: Bug fix time.windows.com
-  Version 2.0: Restructure URI test land include more urls
+  Version 1.7: Restructure URI test land include more urls
+
 #> 
 $ProgressPreference = "SilentlyContinue"
 function Get-NetworkInformation {
@@ -53,23 +54,22 @@ function Get-ComputerInformation {
     $AutopilotCache = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Provisioning\AutopilotPolicyCache" -Name "PolicyJsonCache"
     $AutopilotCache = $AutopilotCache | ConvertFrom-Json
     $APProfileName = $AutopilotCache.DeploymentProfileName
-    $OSEdition = systeminfo.exe
-    $OSEdition = ($OSEdition[2].Replace("OS Name:","").trim()).Replace("Microsoft ","")
+    $OSEdition = (Get-CimInstance win32_operatingsystem).Caption.Replace("Microsoft ","")
     $computerInfo = get-computerinfo
     $tpmInfo = get-tpm
     
-    $windowsVerison = @(
+    $windowsVersion = @(
         "Windows 10 Enterprise", "Windows 10 Education", "Windows 10 Pro for Workstations", "Windows 10 Pro Education", "Windows 10 Pro" ,"Windows 11 Enterprise", "Windows 11 Education", "Windows 11 Pro for Workstations", "Windows 11 Pro Education", "Windows 11 Pro"
     )
     
     Write-Host -NoNewline "  Windows Edition :     "
-    if($windowsVerison.Contains($($OSEdition))){
+    if($windowsVersion.Contains($($OSEdition))){
         Write-Host -ForegroundColor green $OSEdition
     }else{
         Write-Host -ForegroundColor red $OSEdition
     }
-    Write-Host "  Winodws Version :     $($computerInfo.WindowsVersion) $($computerInfo.OSDisplayVersion)"
-    Write-Host "  Winodws InstallDate : $($computerInfo.OsInstallDate)"
+    Write-Host "  Windows Version :     $($computerInfo.WindowsVersion) $($computerInfo.OSDisplayVersion)"
+    Write-Host "  Windows InstallDate : $($computerInfo.OsInstallDate)"
     Write-Host "  Bios Version :        $($computerInfo.BiosBIOSVersion)"
     Write-Host "  Bios Status :         $($computerInfo.BiosStatus)"
     Write-Host "  Bios Serialnumber :   $($computerInfo.BiosSeralNumber)"
@@ -91,74 +91,19 @@ function Get-ComputerInformation {
 }
 
 function Get-ConnectionTest {
+    param(
+		[Parameter(Mandatory)]
+		$connections,
+		
+		[Parameter(Mandatory)]
+		[int]$port
+	)
+
     #443
-    Write-Host -ForegroundColor blue "Test port 443:"
-    @(
-        [pscustomobject]@{uri='www.msftconnecttest.com';Area='Connection test'},
+    Write-Host -ForegroundColor blue "Test port $port :"
 
-        [pscustomobject]@{uri='login.microsoftonline.com';Area='Microsoft authentication'},
-        [pscustomobject]@{uri='aadcdn.msauth.net';Area='Microsoft authentication'},
-
-        [pscustomobject]@{uri='enterpriseregistration.windows.net';Area='Intune'},
-        [pscustomobject]@{uri='enterpriseenrollment-s.manage.microsoft.com';Area='Intune'},
-        [pscustomobject]@{uri='enterpriseEnrollment.manage.microsoft.com';Area='Intune'},
-        [pscustomobject]@{uri='enrollment.manage.microsoft.com';Area='Intune'},
-        [pscustomobject]@{uri='portal.manage.microsoft.com';Area='Intune'},
-        [pscustomobject]@{uri='config.office.com';Area='Intune'},
-        [pscustomobject]@{uri='graph.windows.net';Area='Intune'},
-        [pscustomobject]@{uri='m.manage.microsoft.com';Area='Intune'},
-        [pscustomobject]@{uri='fef.msuc03.manage.microsoft.com';Area='Intune'},
-        [pscustomobject]@{uri='mam.manage.microsoft.com';Area='Intune'},
-        [pscustomobject]@{uri='manage.microsoft.com';Area='Intune'},
-
-        [pscustomobject]@{uri='ztd.dds.microsoft.com';Area='Autopilot Service'},
-        [pscustomobject]@{uri='cs.dds.microsoft.com';Area='Autopilot Service'},
-        [pscustomobject]@{uri='login.live.com';Area='Autopilot Service'},
-
-        [pscustomobject]@{uri='activation.sls.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='licensing.mp.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='validation-v2.sls.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='validation.sls.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='purchase.mp.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='purchase.md.mp.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='licensing.md.mp.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='go.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='displaycatalog.md.mp.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='displaycatalog.mp.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='activation-v2.sls.microsoft.com';Area='License activation'},
-        [pscustomobject]@{uri='activation.sls.microsoft.com';Area='License activation'},
-
-        [pscustomobject]@{uri='emdl.ws.microsoft.com';Area='Windows Update'},
-        [pscustomobject]@{uri='dl.delivery.mp.microsoft.com';Area='Windows Update'},
-        [pscustomobject]@{uri='update.microsoft.com';Area='Windows Update'},
-        [pscustomobject]@{uri='fe2cr.update.microsoft.com';Area='Windows Update'},
-
-        [pscustomobject]@{uri='autologon.microsoftazuread-sso.com';Area='Single sign-on'},
-
-        [pscustomobject]@{uri='powershellgallery.com';Area='Powershell gallery'},
-
-        [pscustomobject]@{uri='ekop.intel.com';Area='TPM check'},
-        [pscustomobject]@{uri='ekcert.spserv.microsoft.com';Area='TPM check'},
-        [pscustomobject]@{uri='ftpm.amd.com';Area='TPM check'},
-
-        [pscustomobject]@{uri='naprodimedatapri.azureedge.net';Area='Powershell and Win32'},
-        [pscustomobject]@{uri='naprodimedatasec.azureedge.net';Area='Powershell and Win32'},
-        [pscustomobject]@{uri='naprodimedatahotfix.azureedge.net';Area='Powershell and Win32'},
-        [pscustomobject]@{uri='euprodimedatapri.azureedge.net';Area='Powershell and Win32'},
-        [pscustomobject]@{uri='euprodimedatasec.azureedge.net';Area='Powershell and Win32'},
-        [pscustomobject]@{uri='euprodimedatahotfix.azureedge.net';Area='Powershell and Win32'},
-        [pscustomobject]@{uri='approdimedatapri.azureedge.net';Area='Powershell and Win32'},
-        [pscustomobject]@{uri='approdimedatasec.azureedge.net';Area='Powershell and Win32'},
-        [pscustomobject]@{uri='approdimedatahotfix.azureedge.net';Area='Powershell and Win32'},
-
-        [pscustomobject]@{uri='v10c.events.data.microsoft.com';Area='Update Compliance'},
-        [pscustomobject]@{uri='v10.vortex-win.data.microsoft.com';Area='Update Compliance'},
-        [pscustomobject]@{uri='settings-win.data.microsoft.com';Area='Update Compliance'},
-        [pscustomobject]@{uri='adl.windows.com';Area='Update Compliance'},
-        [pscustomobject]@{uri='watson.telemetry.microsoft.com';Area='Update Compliance'},
-        [pscustomobject]@{uri='oca.telemetry.microsoft.com';Area='Update Compliance'}       
-    ) | ForEach-Object {
-        $result = (Test-NetConnection -Port 443 -ComputerName $_.uri)    
+    $connections | ForEach-Object {
+        $result = (Test-NetConnection -Port $port -ComputerName $_.uri)    
         Write-Host -NoNewline "  $($_.area): $($result.ComputerName) ($($result.RemoteAddress)): "
         if($result.TcpTestSucceeded) {
             Write-Host -ForegroundColor Green $result.TcpTestSucceeded
@@ -166,27 +111,113 @@ function Get-ConnectionTest {
             Write-Host -ForegroundColor Red $result.TcpTestSucceeded
         }
     }
-
-    #80
-    Write-Host -ForegroundColor blue "Test port 80:"
-    @(
-        [pscustomobject]@{uri='emdl.ws.microsoft.com';Area='Windows Update'},
-        [pscustomobject]@{uri='dl.delivery.mp.microsoft.com';Area='Windows Update'},    
-
-        [pscustomobject]@{uri='time.windows.com';Area='Time service'}
-    ) | ForEach-Object {
-        $result = (Test-NetConnection -Port 80 -ComputerName $_.uri)    
-        Write-Host -NoNewline "  $($_.area): $($result.ComputerName) ($($result.RemoteAddress)): "
-        if($result.TcpTestSucceeded) {
-            Write-Host -ForegroundColor Green $result.TcpTestSucceeded
-        }else{
-            Write-Host -ForegroundColor Red $result.TcpTestSucceeded
-        }
-    }
-
-
     Write-Host
 }
+
+function Get-OtherConnectionsTested {
+    param(
+		[Parameter(Mandatory)]
+		$connections
+	)
+
+    $msEndpoints = @()
+    (invoke-restmethod -Uri ("https://endpoints.office.com/endpoints/WorldWide?ServiceAreas=MEM`&clientrequestid=" + ([GUID]::NewGuid()).Guid)) | ?{$_.ServiceArea -eq "MEM" -and $_.urls} | select -ExpandProperty urls | ForEach-Object {
+        #$msEndpoints += $_.Replace("*.", "")
+        $msEndpoints += $_
+    }
+    $msEndpoints = $msEndpoints | Where-Object {$_ -notmatch "\*." -and $_ -notin $connections}    
+    Write-Host -ForegroundColor blue "Check all other connections (443):"
+
+    $msEndpoints | ForEach-Object {
+        $result = (Test-NetConnection -Port 443 -ComputerName $_)    
+        Write-Host -NoNewline "  Other Connections: $($result.ComputerName) ($($result.RemoteAddress)): "
+        if($result.TcpTestSucceeded) {
+            Write-Host -ForegroundColor Green $result.TcpTestSucceeded
+        }else{
+            Write-Host -ForegroundColor Red $result.TcpTestSucceeded
+        }
+    }
+    Write-Host
+}
+
+
+###########################################################################
+################################# START ###################################
+###########################################################################
+$connections443 = @(
+    [pscustomobject]@{uri='www.msftconnecttest.com';Area='Connection test'},
+
+    [pscustomobject]@{uri='login.microsoftonline.com';Area='Microsoft authentication'},
+    [pscustomobject]@{uri='aadcdn.msauth.net';Area='Microsoft authentication'},
+
+    [pscustomobject]@{uri='enterpriseregistration.windows.net';Area='Intune'},
+    [pscustomobject]@{uri='enterpriseenrollment-s.manage.microsoft.com';Area='Intune'},
+    [pscustomobject]@{uri='enterpriseEnrollment.manage.microsoft.com';Area='Intune'},
+    [pscustomobject]@{uri='enrollment.manage.microsoft.com';Area='Intune'},
+    [pscustomobject]@{uri='portal.manage.microsoft.com';Area='Intune'},
+    [pscustomobject]@{uri='config.office.com';Area='Intune'},
+    [pscustomobject]@{uri='graph.windows.net';Area='Intune'},
+    [pscustomobject]@{uri='m.manage.microsoft.com';Area='Intune'},
+    [pscustomobject]@{uri='fef.msuc03.manage.microsoft.com';Area='Intune'},
+    [pscustomobject]@{uri='mam.manage.microsoft.com';Area='Intune'},
+    [pscustomobject]@{uri='manage.microsoft.com';Area='Intune'},
+
+    [pscustomobject]@{uri='ztd.dds.microsoft.com';Area='Autopilot Service'},
+    [pscustomobject]@{uri='cs.dds.microsoft.com';Area='Autopilot Service'},
+    [pscustomobject]@{uri='login.live.com';Area='Autopilot Service'},
+
+    [pscustomobject]@{uri='activation.sls.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='licensing.mp.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='validation-v2.sls.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='validation.sls.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='purchase.mp.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='purchase.md.mp.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='licensing.md.mp.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='go.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='displaycatalog.md.mp.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='displaycatalog.mp.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='activation-v2.sls.microsoft.com';Area='License activation'},
+    [pscustomobject]@{uri='activation.sls.microsoft.com';Area='License activation'},
+
+    [pscustomobject]@{uri='emdl.ws.microsoft.com';Area='Windows Update'},
+    [pscustomobject]@{uri='dl.delivery.mp.microsoft.com';Area='Windows Update'},
+    [pscustomobject]@{uri='update.microsoft.com';Area='Windows Update'},
+    [pscustomobject]@{uri='fe2cr.update.microsoft.com';Area='Windows Update'},
+
+    [pscustomobject]@{uri='autologon.microsoftazuread-sso.com';Area='Single sign-on'},
+
+    [pscustomobject]@{uri='powershellgallery.com';Area='Powershell gallery'},
+
+    [pscustomobject]@{uri='ekop.intel.com';Area='TPM check'},
+    [pscustomobject]@{uri='ekcert.spserv.microsoft.com';Area='TPM check'},
+    [pscustomobject]@{uri='ftpm.amd.com';Area='TPM check'},
+
+    [pscustomobject]@{uri='naprodimedatapri.azureedge.net';Area='Powershell and Win32'},
+    [pscustomobject]@{uri='naprodimedatasec.azureedge.net';Area='Powershell and Win32'},
+    [pscustomobject]@{uri='naprodimedatahotfix.azureedge.net';Area='Powershell and Win32'},
+    [pscustomobject]@{uri='euprodimedatapri.azureedge.net';Area='Powershell and Win32'},
+    [pscustomobject]@{uri='euprodimedatasec.azureedge.net';Area='Powershell and Win32'},
+    [pscustomobject]@{uri='euprodimedatahotfix.azureedge.net';Area='Powershell and Win32'},
+    [pscustomobject]@{uri='approdimedatapri.azureedge.net';Area='Powershell and Win32'},
+    [pscustomobject]@{uri='approdimedatasec.azureedge.net';Area='Powershell and Win32'},
+    [pscustomobject]@{uri='approdimedatahotfix.azureedge.net';Area='Powershell and Win32'},
+
+    [pscustomobject]@{uri='v10c.events.data.microsoft.com';Area='Update Compliance'},
+    [pscustomobject]@{uri='v10.vortex-win.data.microsoft.com';Area='Update Compliance'},
+    [pscustomobject]@{uri='settings-win.data.microsoft.com';Area='Update Compliance'},
+    [pscustomobject]@{uri='adl.windows.com';Area='Update Compliance'},
+    [pscustomobject]@{uri='watson.telemetry.microsoft.com';Area='Update Compliance'},
+    [pscustomobject]@{uri='oca.telemetry.microsoft.com';Area='Update Compliance'}       
+)
+
+$connections80 = @(
+    [pscustomobject]@{uri='emdl.ws.microsoft.com';Area='Windows Update'},
+    [pscustomobject]@{uri='dl.delivery.mp.microsoft.com';Area='Windows Update'},    
+
+    [pscustomobject]@{uri='time.windows.com';Area='Time service'}
+)
+
+
 
 Write-Host -ForegroundColor Yellow "######################################"
 Write-Host -ForegroundColor Yellow "# Start Autopilot prerequisite check #"
@@ -205,7 +236,9 @@ Write-Host
 Write-Host -ForegroundColor Yellow "---------------------------------"
 Write-Host -ForegroundColor Yellow "|        Connection Test        |"
 Write-Host -ForegroundColor Yellow "---------------------------------"
-Get-ConnectionTest
+Get-ConnectionTest -connections $connections443 -port 443
+Get-ConnectionTest -connections $connections80 -port 80
+Get-OtherConnectionsTested -connections ($connections80 + $connections443).uri
 Write-Host
 Write-Host -ForegroundColor Yellow "######################################"
 Write-Host -ForegroundColor Yellow "#  Autopilot prerequisite check Done #"
