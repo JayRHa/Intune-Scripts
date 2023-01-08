b480b07c537b6213d844afb56e89eb5af4ab42c1
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2023-01-08 13:51:50 +0100

    Improve

--- Get-MdmDiagnostigLogs/Get-MdmDiagnosticLogs.ps1
+++ Get-MdmDiagnostigLogs/Get-MdmDiagnosticLogs.ps1
@@ -1,11 +1,12 @@
+# ToDo:Add header
+
 param (
-  [parameter(HelpMessage = "Collect new logs")]
-  [ValidateNotNullOrEmpty()]
-  [switch]$collectNew=$false,
-  [parameter(Mandatory=$true, HelpMessage = "Typle of logs")]
-  [ValidateSet('all','systemInformation', 'enrollment', 'devMgmtAccount', 'policies')]
-  [string[]]$output="all"
+  [parameter(HelpMessage = "Collect new logs")][ValidateNotNullOrEmpty()][switch]$collectNew=$false,
+  [parameter(HelpMessage = "Get return only")][switch]$returnonly=$false,
+  [parameter(Mandatory=$true, HelpMessage = "Typle of logs")][ValidateSet('All','ActiveSync','DeviceManageabilityProviderInfo','DeviceManagementAccount','Diagnostics','EAS','Enrollments','EnterpriseDesktopAppManagementinfo','FirstSyncData','MdmWinsOverGp','PolicyManager','PolicyManagerMeta','ProvisioningResults','Resources','SCEP','SystemInformation','Version','WAP')][string[]]$output="all"
 )
+
+
 
 #################################################################################################
 ######################################## Function ###############################################
@@ -17,7 +18,7 @@ Function Get-CheckIfLogExist{
 
 Function Write-InitHeader {
     param(
-      [string]$header,
+      [string]$header
     )
   $lenght = $header.Length + 33
   
@@ -28,6 +29,85 @@ Function Write-InitHeader {
   Write-Host
 }
 
+function Get-ObjectPrinted
+{
+  # https://www.red-gate.com/simple-talk/blogs/display-object-a-powershell-utility-cmdlet/
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true)]
+		$TheObject,
+		[int]$depth = 5,
+		[Object[]]$Avoid = @('#comment'),
+		[string]$Parent = '$',
+		[int]$CurrentDepth = 0
+	)
+	
+	if (($CurrentDepth -ge $Depth) -or
+		($TheObject -eq $Null)) { return; }
+	$ObjectTypeName = $TheObject.GetType().Name
+	if ($ObjectTypeName -in 'HashTable', 'OrderedDictionary')
+	{
+		$TheObject = [pscustomObject]$TheObject;
+		$ObjectTypeName = 'PSCustomObject'
+	}
+	if ($TheObject.Count -le 1 -and $ObjectTypeName -ne 'object[]')
+	{
+		if ($ObjectTypeName -in @('PSCustomObject'))
+		{ $MemberType = 'NoteProperty' }
+		else
+		{ $MemberType = 'Property' }
+		$TheObject | 
+		gm -MemberType $MemberType | where { $_.Name -notin $Avoid } |
+		Foreach{
+			Try { $child = $TheObject.($_.Name); }
+			Catch { $Child = $null }
+            $brackets=''; if ($_.Name -like '*.*'){$brackets="'"}
+			if ($child -eq $null -or
+				$child.GetType().BaseType.Name -eq 'ValueType' -or
+				$child.GetType().Name -in @('String', 'String[]'))
+			{ [pscustomobject]@{ 'Path' = "$Parent.$brackets$($_.Name)$brackets"; 'Value' = $Child; } }
+			elseif (($CurrentDepth + 1) -eq $Depth)
+			{
+				[pscustomobject]@{ 'Path' = "$Parent.$brackets$($_.Name)$brackets"; 'Value' = $Child; }
+			}
+			else
+			{
+				Get-ObjectPrinted -TheObject $child -depth $Depth -Avoid $Avoid `
+                               -Parent "$Parent.$brackets$($_.Name)$brackets" `
+							   -CurrentDepth ($currentDepth + 1)
+			}
+	
+		}
+	}
+	else
+	{
+		if ($TheObject.Count -gt 0)
+            {0..($TheObject.Count - 1) | Foreach{
+			$child = $TheObject[$_];
+			if (($child -eq $null) -or
+				($child.GetType().BaseType.Name -eq 'ValueType') -or
+				($child.GetType().Name -in @('String', 'String[]')))
+			{ [pscustomobject]@{ 'Path' = "$Parent[$_]"; 'Value' = "$($child)"; } }
+			elseif (($CurrentDepth + 1) -eq $Depth)
+			{
+				[pscustomobject]@{ 'Path' = "$Parent[$_]"; 'Value' = "$($child)"; }
+			}
+			else
+			{
+				Get-ObjectPrinted -TheObject $child -depth $Depth -Avoid $Avoid -parent "$Parent[$_]" `
+							   -CurrentDepth ($currentDepth + 1)
+			}
+			
+		}
+	}
+    else {[pscustomobject]@{ 'Path' = "$Parent"; 'Value' = $Null }}
+    }
+}
+
+
+
 Function Write-ValueOutFromObject{
   param(
     [string]$header,
@@ -35,170 +115,65 @@ Function Write-ValueOutFromObject{
   )
 
   Write-InitHeader -header $header
-  
-  $object.PSObject.Properties | ForEach-Object {
-    Write-Host "- $($_.Name): " -NoNewline -ForegroundColor Black
-    Write-Host $_.Value
+  Get-ObjectPrinted $object -depth 6
+}
+
+function Convert-XmlNodeToPsCustomObject ($node){
+  # https://stackoverflow.com/questions/3242995/convert-xml-to-psobject
+  $hash = @{}
+  foreach($attribute in $node.attributes){
+      $hash.$($attribute.name) = $attribute.Value
+  }
+  $childNodesList = ($node.childnodes | ?{$_ -ne $null}).LocalName
+  foreach($childnode in ($node.childnodes | ?{$_ -ne $null})){
+      if(($childNodesList | ?{$_ -eq $childnode.LocalName}).count -gt 1){
+          if(!($hash.$($childnode.LocalName))){
+              $hash.$($childnode.LocalName) += @()
+          }
+          if ($childnode.'#text' -ne $null) {
+              $hash.$($childnode.LocalName) += $childnode.'#text'
+          }
+          $hash.$($childnode.LocalName) += xmlNodeToPsCustomObject($childnode)
+      }else{
+          if ($childnode.'#text' -ne $null) {
+              $hash.$($childnode.LocalName) = $childnode.'#text'
+          }else{
+              $hash.$($childnode.LocalName) = xmlNodeToPsCustomObject($childnode)
+          }
+      }   
+  }
+  return $hash | ConvertTo-PsCustomObjectFromHashtable
+}
+
+Function ConvertTo-PsCustomObjectFromHashtable { 
+  param ( 
+      [Parameter(  
+          Position = 0,   
+          Mandatory = $true,   
+          ValueFromPipeline = $true,  
+          ValueFromPipelineByPropertyName = $true  
+      )] [object[]]$hashtable 
+  ); 
+
+  begin { $i = 0; } 
+
+  process { 
+      foreach ($myHashtable in $hashtable) { 
+          if ($myHashtable.GetType().Name -eq 'hashtable') { 
+              $output = New-Object -TypeName PsObject; 
+              Add-Member -InputObject $output -MemberType ScriptMethod -Name AddNote -Value {  
+                  Add-Member -InputObject $this -MemberType NoteProperty -Name $args[0] -Value $args[1]; 
+              }; 
+              $myHashtable.Keys | Sort-Object | % {  
+                  $output.AddNote($_, $myHashtable.$_);  
+              } 
+              $output
+          } else { 
+              Write-Warning "Index $i is not of type [hashtable]"; 
+          }
+          $i += 1;  
+      }
   } 
-}
-
-Function Get-MdmSystemInformation{
-  param([xml]$xmlLog)
-
-  return = [PSCustomObject]@{
-    "Report Creation Time" = $xmlLog.MDMEnterpriseDiagnosticsReport.SystemInformation.ReportCreationTime
-    "OS Version" =  $xmlLog.MDMEnterpriseDiagnosticsReport.SystemInformation.OSVersion
-    "Serial number" = $xmlLog.MDMEnterpriseDiagnosticsReport.SystemInformation.SerialNumber
-    "Build Branch" = $xmlLog.MDMEnterpriseDiagnosticsReport.SystemInformation.BuildBranch
-  }
-}
-
-Function Get-MdmEnrollmentInfo{
-  param([xml]$xmlLog)
-  $enrollment = ($xmlLog.MDMEnterpriseDiagnosticsReport.Enrollments | Where-Object {$_.DMServerCertificateThumbprint})[0]
-
-  return = [PSCustomObject]@{
-    "Enrollment Id" = $enrollment.EnrollmentId
-    "Enrollment Syste" = $enrollment.EnrollmentState
-    "Enrollment Type" = $enrollment.EnrollmentType
-    "Cur Crypto Provider" = $enrollment.CurCryptoProvider
-    "Discovery Service FullURL" = $enrollment.DiscoveryServiceFullURL
-    "DM Server Certificate Thumbprint" = $enrollment.DMServerCertificateThumbprint
-    "Is Federated" = $enrollment.IsFederated
-    "Provider ID" = $enrollment.ProviderID
-    "Renewal Period" = $enrollment.RenewalPeriod
-    "Renewal Error Code" = $enrollment.RenewalErrorCode
-    "Renewal ROBO Support" = $enrollment.RenewalROBOSupport
-    "Renewal Status" = $enrollment.RenewalStatus
-    "Retry Interval" = $enrollment.RetryInterval
-    "Root Certificate ThumbPrint" = $enrollment.RootCertificateThumbPrint
-
-    "DM Client Servername" = $enrollment.DMClient.DMClientServerName
-    "Cert Renew Timestamp" = $enrollment.DMClient.CertRenewTimeStamp
-    "Enterprise Devicename" = $enrollment.DMClient.EnterpriseDeviceName
-    "Enterprise DeviceManagement ID" = $enrollment.DMClient.EnterpriseDeviceManagementID
-    "Signed Enterprise DeviceManagement ID" = $enrollment.DMClient.SignedEnterpriseDeviceManagementID
-
-    "Poll Settings" = $enrollment.Poll.PollSettings
-    "Aux Retry Interval" = $enrollment.Poll.AuxRetryInterval
-    "Aux Num Retries" = $enrollment.Poll.AuxNumRetries
-    "Retry Interval" = $enrollment.Poll.RetryInterval
-    "Num Retries" = $enrollment.Poll.NumRetries
-    "Aux2 Retry Interval" = $enrollment.Poll.Aux2RetryInterval
-    "Aux2 Num Retries" = $enrollment.Poll.Aux2NumRetries
-    "Poll On Login" = $enrollment.Poll.PollOnLogin
-    "All Users Poll On First Login" = $enrollment.Poll.AllUsersPollOnFirstLogin
-
-    "Device First Sync Status" = $enrollment.FirstSync.DeviceFirstSyncStatus
-    "Sync Failure Timeout" = $enrollment.FirstSync.SyncFailureTimeout
-    "Block In Status Page" = $enrollment.FirstSync.BlockInStatusPage
-    "Skip Device Status Page" = $enrollment.FirstSync.SkipDeviceStatusPage
-    "Skip User Status Page" = $enrollment.FirstSync.SkipUserStatusPage
-    "Allow Collect Logs Button" = $enrollment.FirstSync.AllowCollectLogsButton
-    "Timestamp" = $enrollment.FirstSync.Timestamp
-    "Policy Duration" = $enrollment.FirstSync.PolicyDuration
-    "Certificates Duration" = $enrollment.FirstSync.CertificatesDuration
-    "Applications Duration" = $enrollment.FirstSync.ApplicationsDuration
-    "Networking Duration" = $enrollment.FirstSync.NetworkingDuration
-    "Is Server Provisioning Done" = $enrollment.FirstSync.IsServerProvisioningDone
-    "Provisioning Status" = $enrollment.FirstSync.ProvisioningStatus
-    "Is Sync Done" = $enrollment.FirstSync.IsSyncDone
-
-    "User SID" = $enrollment.UserFirstSync.UserSID
-
-    "Push" = $enrollment.Push.Push
-    "PFN" = $enrollment.Push.PFN
-    "Channel URI" = $enrollment.Push.ChannelURI
-    "Status" = $enrollment.Push.Status
-    "Device Channel" = $enrollment.Push.DeviceChannel
-    "Retry Count" = $enrollment.Push.RetryCount
-  }
-}
-
-Function Get-MdmDeviceManagementAccount{
-  param([xml]$xmlLog)
-  $deviceManagementAccount = $xmlLog.MDMEnterpriseDiagnosticsReport.DeviceManagementAccount.Enrollment
-
-  return = [PSCustomObject]@{
-    "Enrollment Id" = $deviceManagementAccount.EnrollmentId
-    "Account UID" = $deviceManagementAccount.AccountUID
-    "Flags" = $deviceManagementAccount.Flags
-    "OMADM Protocol Version" = $deviceManagementAccount.OMADMProtocolVersion
-    "Roaming Count" = $deviceManagementAccount.RoamingCount
-    "MDM Server Version" = $deviceManagementAccount.MDMServerVersion
-    "Ssl Client Cert Reference" = $deviceManagementAccount.SslClientCertReference
-
-    "Protected Data" = $deviceManagementAccount.ProtectedInformation.ProtectedData
-    "DMAccount Root Name" = $deviceManagementAccount.ProtectedInformation.DMAccountRootName
-    "Application ID" = $deviceManagementAccount.ProtectedInformation.ApplicationID
-    "Authentication Preference" = $deviceManagementAccount.ProtectedInformation.AuthenticationPreference
-    "Default Encoding" = $deviceManagementAccount.ProtectedInformation.DefaultEncoding
-    "Mode" = $deviceManagementAccount.ProtectedInformation.Mode
-    "MDM Server Name" = $deviceManagementAccount.ProtectedInformation.MDMServerName
-    "Roles" = $deviceManagementAccount.ProtectedInformation.Roles
-    "Provider ID" = $deviceManagementAccount.ProtectedInformation.ProviderID
-    "Ssl Client Cert Search Criteria" = $deviceManagementAccount.ProtectedInformation.SslClientCertSearchCriteria
-
-    "Address Information" = $deviceManagementAccount.ProtectedInformation.AddressInformation.AddressInformation
-    "Address" = $deviceManagementAccount.ProtectedInformation.AddressInformation.Address
-    "Address Root Name" = $deviceManagementAccount.ProtectedInformation.AddressInformation.AddressRootName
-    "Address Type" = $deviceManagementAccount.ProtectedInformation.AddressInformation.AddressType
-    "Flags" = $deviceManagementAccount.ProtectedInformation.AddressInformation.Flags
-
-    "AuthInfo ID" = $deviceManagementAccount.ProtectedInformation.AuthenticationInformation.AuthInfoID
-    "Authentication Level" = $deviceManagementAccount.ProtectedInformation.AuthenticationInformation.AuthenticationLevel
-    "Authentication Root Name" = $deviceManagementAccount.ProtectedInformation.AuthenticationInformation.AuthenticationRootName
-    "Authentication Secret" = $deviceManagementAccount.ProtectedInformation.AuthenticationInformation.AuthenticationSecret
-    "Authentication Type" = $deviceManagementAccount.ProtectedInformation.AuthenticationInformation.AuthenticationType
-    
-    "ConnectionInfo ID" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.ConnectionInfoID
-    "Back Compat Retry Disabled" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.BackCompatRetryDisabled
-    "Conn Retry Freq" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.ConnRetryFreq
-    "Flags" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.Flags
-    "Initial Back Off Time" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.InitialBackOffTime
-    "Last Session Result" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.LastSessionResult
-    "Max Back Off Time" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.MaxBackOffTime
-    "Server Last Access Time" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.ServerLastAccessTime
-    "Server Last Failure Time" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.ServerLastFailureTime
-    "Server Last Success Time" = $deviceManagementAccount.ProtectedInformation.ConnectionInformation.ServerLastSuccessTime
-  }
-}
-
-
-Function Get-MdmPolicies{
-  param([xml]$xmlLog)
-  $policies = $xmlLog.MDMEnterpriseDiagnosticsReport.PolicyManager
-
-  $allPolicies =  @()
-  $policies | ForEach {
-    $allPolicies += [PSCustomObject]@{
-      "Enrollment Id" = $_.ConfigSource.EnrollmentId
-      "Scope" = $_.ConfigSource.PolicyScope.PolicyScope
-      "Area" = $_.ConfigSource.PolicyScope.Area
-      #ToDo: Split area
-    }
-  }
-  return $allPolicies
-}
-
-Function Get-MdmRessources{
-  param([xml]$xmlLog)s
-  #? For each scope
-  $ressources = $xmlLog.MDMEnterpriseDiagnosticsReport.Resources.Enrollment.Scope.Resources
-  #ToDo: For each 
-
-}
-
-Function Get-MdmEas{
-  param([xml]$xmlLog)
-  $eas = $xmlLog.MDMEnterpriseDiagnosticsReport.EAS.EASPolicies
-
-  return = [PSCustomObject]@{
-    "Type" = $eas.Type
-    "Min Device Password Complex Characters" = $eas.MinDevicePasswordComplexCharacters
-    "Min Device Password Length" = $eas.MinDevicePasswordLength
-    "Max Device Password Failed Attempts" = $eas.MaxDevicePasswordFailedAttempts
-  }
 }
 
 #################################################################################################
@@ -209,25 +184,23 @@ if(-not ((Get-CheckIfLogExist -path $logPath) -or $collectNew)){
   MdmDiagnosticsTool.exe -out $logPath
 }
 
-[xml]$logFile = Get-Content -Path "$logPath/MDMDiagReport.xml"
+[xml]$xmlLog = Get-Content -Path "$logPath/MDMDiagReport.xml"
+$mdmObject = Convert-XmlNodeToPsCustomObject $xmlLog
 
-# Write output
-#### System Information ###
-if($output -contains "all" -or $output -contains "systemInformation") {
-  Write-ValueOutFromObject -header "System Information" -object $(Get-MdmSystemInformation -xmlLog $logFile)
-}
-
-if($output -contains "all" -or $output -contains "enrollment") {
-  Write-ValueOutFromObject -header "Enrollment" -object $(Get-MdmEnrollmentInfo -xmlLog $logFile)
-}
-
-
-if($output -contains "all" -or $output -contains "devMgmtAccount") {
-  Write-ValueOutFromObject -header "Device Management Account" -object $(Get-MdmDeviceManagementAccount -xmlLog $logFile)
-}
-
-if($output -contains "all" -or $output -contains "policies") {
-  Write-InitHeader -header "Policies"
-
+if($returnonly){
+  if($output -contains "all"){
+    return ($mdmObject.MDMEnterpriseDiagnosticsReport)
+  }else{
+      # ToDo: Optimize
+    ($mdmObject.MDMEnterpriseDiagnosticsReport).PSObject.Properties | Where-Object {$_.Name -in $output}
+  }
+}else{
+  ($mdmObject.MDMEnterpriseDiagnosticsReport).PSObject.Properties | ForEach-Object {
+    if($output -contains "all"){
+      Write-ValueOutFromObject -header $_.Name -object $_
+    }elseif($output -contains $_.Name){
+        Write-ValueOutFromObject -header $_.Name -object $_
+    }
+  }
 }
 
