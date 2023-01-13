547f892728b9cc9e6d24e485b48444bb39853533
Jannik Reinhard <73911860+JayRHa@users.noreply.github.com>
2023-01-13 18:54:13 +0100

    Update Get-PendingRebootDetection.ps1

--- Get-PendingReboot/Get-PendingRebootDetection.ps1
+++ Get-PendingReboot/Get-PendingRebootDetection.ps1
@@ -1,4 +1,14 @@
-If(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"){
+If(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired")
+{
     Exit 1
 }
+ElseIf(test-path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending")
+{
+    Exit 1
+}
+ElseIf(test-path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootInProgress")
+{
+    Exit 1
+}
+
 Exit 0
